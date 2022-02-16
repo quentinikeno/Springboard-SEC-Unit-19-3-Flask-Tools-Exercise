8b66c940b0388de43f18fd1b3061244f44eb4d80
Quentin Ikeno <ikenoq@gmail.com>
2022-02-15 17:32:28 -1000

    Add new routes to pick a survey

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import surveys
 
 app = Flask(__name__)
 
@@ -11,25 +11,37 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def index():
     """Show homepage to start survey."""
-    title = satisfaction_survey.title
-    instructions = satisfaction_survey.instructions
-    return render_template('index.html', title=title, instructions=instructions)
+    return render_template('index.html', surveys=surveys.keys())
+
+@app.route('/survey-select', methods=['POST'])
+def pick_survey():
+    #Store selected survey name (key in surveys dictionary) in session
+    #Use this to access the appropriate survey
+    session['survey'] =  request.form.get('surveys')
+    return redirect('/start-page')
+
+@app.route('/start-page')
+def show_start_page():
+    """Show homepage to start survey."""
+    survey = surveys[session['survey']]
+    title = survey.title
+    instructions = survey.instructions
+    return render_template('start_page.html', title=title, instructions=instructions)
 
 @app.route('/init-survey', methods=['POST'])
 def intialize_survey():
     #Store responses as a list in session
     session['responses'] = []
     return redirect('/questions/0')
-
-responses = []
  
 @app.route('/questions/<int:q_number>')
 def show_question(q_number):
     """Show the current question."""
+    survey = surveys[session['survey']]
     num_responses = len(session['responses'])
 
     #Redirect to thank you page if all questions are answered
-    if(num_responses == len(satisfaction_survey.questions)):
+    if(num_responses == len(survey.questions)):
         flash('You have already completed the survey.', 'warning')
         return redirect('/thank-you')
 
@@ -39,14 +51,15 @@ def show_question(q_number):
         flash('You are trying to answer an invalid question.  Please answer the question below.', 'error')
         return redirect(f'/questions/{num_responses}')
     
-    question = satisfaction_survey.questions[q_number]
-    title = satisfaction_survey.title
+    question = survey.questions[q_number]
+    title = survey.title
     return render_template('question.html', title=title, question=question, q_number=q_number)
 
 @app.route('/answers', methods=['POST'])
 def submit_answer():
     """Add answer to responses list and redirect."""
     q_number = int(request.form['q_number'])
+    survey = surveys[session['survey']]
     ans = request.form.get('response')
     #rebind responses name from session
     responses = session['responses']
@@ -54,7 +67,7 @@ def submit_answer():
     #Update responses in session
     session['responses'] = responses
 
-    if(q_number + 1 == len(satisfaction_survey.questions)):
+    if(q_number + 1 == len(survey.questions)):
         #Redirect to thank you page if answered last question
         return redirect('/thank-you')
 
@@ -62,5 +75,6 @@ def submit_answer():
 
 @app.route('/thank-you')
 def show_thank_you():
-    title = satisfaction_survey.title
+    survey = surveys[session['survey']]
+    title = survey.title
     return render_template('thank_you.html', title=title)
